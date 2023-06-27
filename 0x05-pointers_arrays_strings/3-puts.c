d5465f60c17938ce73d53553b265c344eb743a48
Your Name <you@example.com>
2023-06-27 14:40:04 -0700

    j

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
- * _puts - Prints a string, followed by a new line, to stdout.
- * @str: The string to be printed.
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
 void _puts(char *str)
 {
-    while (*str)
-    {
-        _putchar(*str);
-        str++;
-    }
-
-    _putchar('\n');
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
 }
+
 
