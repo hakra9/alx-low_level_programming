9d7187d45f1ea82560367bba4bff66714b350d8a
Your Name <you@example.com>
2023-06-27 14:51:56 -0700

    h

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,42 +1,48 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: The converted integer.
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
+	int i, d, n, len, f, digit;
 
-    /* Handle signs */
-    if (s[0] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-    else if (s[0] == '+')
-    {
-        i++;
-    }
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-    /* Process digits */
-    while (s[i] != '\0')
-    {
-        /* Check for non-digit characters */
-        if (s[i] < '0' || s[i] > '9')
-        {
-            break;
-        }
+	while (s[len] != '\0')
+		len++;
 
-        /* Convert character to integer */
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
 
-    return sign * result;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
+
 
