2e61de966e356c529eabce31efb669916c2b5020
Your Name <you@example.com>
2023-06-29 13:01:05 -0700

    g

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,18 +3,20 @@
  * reverse_array - reverse array of integers
  * @a: array
  * @n: number of elements of array
+ *
  * Return: void
  */
 void reverse_array(int *a, int n)
 {
-	int j;
-	int l;
+	int i;
+	int t;
 
-	for (j = 0; j < n--; j++)
+	for (i = 0; i < n--; i++)
 	{
-		l = a[j];
-		a[j] = a[n];
-		a[n] =l;
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
 }
+
 
