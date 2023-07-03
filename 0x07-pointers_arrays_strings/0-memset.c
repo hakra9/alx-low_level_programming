1221d52deef8d11f24ca29f7f4b179f551e621ef
Your Name <you@example.com>
2023-07-03 11:32:58 -0700

    h

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,18 +4,19 @@
  * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
-*
+ *
  * Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int j = 0;
+	int i = 0;
 
-	for (; n > 0; j++)
+	for (; n > 0; i++)
 	{
-		s[j] = b;
+		s[i] = b;
 		n--;
 	}
 	return (s);
 }
+
 
