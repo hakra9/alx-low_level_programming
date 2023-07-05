71318b2b397ad348bda0e8f49d76dae6f97fde4a
Your Name <you@example.com>
2023-07-05 09:54:36 -0700

    h

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,18 +4,14 @@
  *@dest: memory where is stored
  *@src: memory where is copied
  *@n: number of bytes
- *
  *Return: copied memory with n bytes changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
     unsigned int r;
-
     for (r = 0; r < n; r++)
     {
         dest[r] = src[r];
     }
-
     return dest;
 }
-
