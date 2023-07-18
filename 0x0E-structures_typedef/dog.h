6807d5a760614424c754218a2e70a5fdf63d060c
Your Name <you@example.com>
2023-07-18 05:16:43 -0700

    j

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,34 @@
 #include <stdio.h>
-struct dog
-{	char * name;
+
+/**
+ * struct dog - Represents a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+typedef struct dog
+{
+	char *name;
 	float age;
-	char * owner;
-}
+	char *owner;
+} dog_t;
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
 int main(void)
-{	struct dog x ;
-	x.name = put ;
-	x.age = 2;
-	x.owner =sam;
-	return (0) ;
+{
+	dog_t myDog;
+
+	myDog.name = "Buddy";
+	myDog.age = 3.5;
+	myDog.owner = "John Smith";
+
+	printf("Dog's name: %s\n", myDog.name);
+	printf("Dog's age: %.1f\n", myDog.age);
+	printf("Dog's owner: %s\n", myDog.owner);
+
+	return 0;
 }
