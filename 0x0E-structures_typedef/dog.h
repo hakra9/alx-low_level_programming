8317700bbc480e9009dad1917fb38aab0f124a50
Your Name <you@example.com>
2023-07-18 05:36:15 -0700

    j

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,34 +1,32 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - Represents a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 /**
- * main - Entry point of the program.
- *
- * Return: Always 0.
+ * dog_t - typedef for struct dog
  */
-int main(void)
-{
-	dog_t myDog;
+typedef struct dog dog_t;
 
-	myDog.name = "Buddy";
-	myDog.age = 3.5;
-	myDog.owner = "John Smith";
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-	printf("Dog's name: %s\n", myDog.name);
-	printf("Dog's age: %.1f\n", myDog.age);
-	printf("Dog's owner: %s\n", myDog.owner);
+#endif
 
-	return 0;
-}
