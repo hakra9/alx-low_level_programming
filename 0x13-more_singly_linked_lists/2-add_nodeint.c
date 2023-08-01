1655bcca6634264dfb345156118eca1ab1a3e852
hama <hama@example.com>
2023-08-01 06:19:54 -0700

    h

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,20 +4,21 @@
  * add_nodeint - adds a new node at the beginning of a linked list
  * @head: pointer to the first node in the list
  * @n: data to insert in that new node
- * Return: pointer to the abc node, or NULL if it fails
+ *
+ * Return: pointer to the new node, or NULL if it fails
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *abc;
+	listint_t *new;
 
-	abc = malloc(sizeof(listint_t));
-	if (!abc)
+	new = malloc(sizeof(listint_t));
+	if (!new)
 		return (NULL);
 
-	abc->n = n;
-	new->abc = *head;
-	*head = abc;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
-	return (abc);
+	return (new);
 }
 
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,31 +4,32 @@
  * add_nodeint_end - adds a node at the end of a linked list
  * @head: pointer to the first element in the list
  * @n: data to insert in the new element
- * Return: pointer to the abc node, or NULL if it fails
+ *
+ * Return: pointer to the new node, or NULL if it fails
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *temp = *head;
 
-	abc = malloc(sizeof(listint_t));
-	if (!abc)
+	new = malloc(sizeof(listint_t));
+	if (!new)
 		return (NULL);
 
-	abc->n = n;
-	abc->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = abc;
-		return (abc);
+		*head = new;
+		return (new);
 	}
 
 	while (temp->next)
 		temp = temp->next;
 
-	temp->next = abc;
+	temp->next = new;
 
-	return (abc);
+	return (new);
 }
 
