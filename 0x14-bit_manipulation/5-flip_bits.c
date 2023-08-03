a0c309c60a8202f0c6da8923da648a9bedf471a8
hama <hama@example.com>
2023-08-03 10:20:37 -0700

    h

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,8 +8,8 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = m ^ n;
-	int num;
+	unsigned long int xor = n ^ m;
+	unsigned int num=0;
 
 	while (xor)
 		{
