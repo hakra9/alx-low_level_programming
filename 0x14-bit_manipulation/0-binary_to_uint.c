521e26434f5a794872aa992ff31c400e0f1c5940
hama <hama@example.com>
2023-08-03 07:49:05 -0700

    j

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/*binary_to_uint - return the number of binaru code
+
+
+/*
+ * binary_to_uint - return the number of binaru code
  * @nu : the number
- * returne : the number converte
+ * return : the number converte
  */
 
 unsigned int binary_to_uint(const char *b);
 {
-	unsigned int nu= 0 ;
+	unsigned int nu = 0 ;
 	if (!b)
 		return (0);
-	while(*b)
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		nu= nu * 2 + (*b++ -'0');
+		nu = nu * 2 + (*b++ - '0');
 	}
 	return (nu);
 }
