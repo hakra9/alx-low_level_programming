6a78ff4a448fdb768b13ea39e7552fb6900d5968
hama <hama@example.com>
2023-08-03 07:51:32 -0700

    j

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b);
 		return (0);
 	while (*b)
 	{
-		if (*b != '0' && *b != '1')
+		if (*b != '0' || *b != '1')
 			return (0);
 		nu = nu * 2 + (*b++ - '0');
 	}
